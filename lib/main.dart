9ff753daba649f695419f9ed973776db01d74014
projectsforchannel <projectsforchannel@gmail.com>
2021-05-13 23:19:37 +0530

    initial

---
+++ lib/main.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:notification/pushNotification.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Notification Test'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+  FirebaseNotifcation firebase;
+
+  handleAsync() async {
+    await firebase.initialize();
+
+    String token = await firebase.getToken();
+    print("Firebase token : $token");
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    firebase = FirebaseNotifcation();
+    handleAsync();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            RaisedButton(
+                color: Colors.blue,
+                onPressed: () {
+                  print("here");
+                  firebase.subscribeToTopic('notify');
+                },
+                child: Text("Subscribe"))
+          ],
+        ),
+      ),
+    );
+  }
+}
